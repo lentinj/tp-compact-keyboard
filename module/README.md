411d3ae9374ed720e5b9ba924f18e12ddfb0d1ab
Jamie Lentin <jm@lentin.co.uk>
2014-03-08 12:19:56 +0000

    Add custom keys to usage map

--- module/README.md
+++ module/README.md
@@ -9,6 +9,13 @@ Then build:
 
 $ make
 
+For debugging:
+
+# insmod hid-lenovo-tpcompactkbd.ko
+(connect the keyboard, should see a connecting device like 0005:17EF:6048.004B)
+# echo -n "0005:17EF:6048.004B" > /sys/bus/hid/drivers/hid-generic/unbind
+# echo -n "0005:17EF:6048.004B" > /sys/bus/hid/drivers/lenovo_tpcompactkbd/bind
+
 
 Notes on creating
 -----------------
@@ -16,3 +23,7 @@ Notes on creating
 Since this isn't a USB device, we can't as easily see the Vendor ID / Product
 ID, which we need to find our keyboard. To do this, use the ``hid-example``
 tool included with the linux kernel.
+
+
+
+http://unix.stackexchange.com/questions/12005/how-to-use-linux-kernel-driver-bind-unbind-interface-for-usb-hid-devices
--- module/hid-lenovo-tpcompactkbd.c
+++ module/hid-lenovo-tpcompactkbd.c
@@ -12,6 +12,7 @@
  * any later version.
  */
 
+#include <linux/kernel.h> /*TODO: For debugging */
 #include <linux/device.h>
 #include <linux/hid.h>
 #include <linux/module.h>
@@ -23,16 +24,16 @@
  * Keyboard sends non-standard reports for most "hotkey" Fn functions.
  * Map these back to regular keys.
  *
- * Esc:	KEY_FN_ESC		FnLock
+ * Esc:	KEY_FN_ESC		FnLock [TODO: Not mapped]
  * (F1--F3 are regular keys)
  * F4:	KEY_MICMUTE		Mic Mute
  * F5:	KEY_BRIGHTNESSDOWN	Brightness down
  * F6:	KEY_BRIGHTNESSUP	Brightness up
- * F7:	KEY_SWITCHVIDEOMODE	External display (projector)
- * F8:	KEY_WWAN		Wireless
- * F9:	KEY_CONFIG		Control panel / settings
+ * F7:	KEY_SWITCHVIDEOMODE	External display (projector) [TODO: Win-P, SwMode]
+ * F8:	KEY_FN_F8		Wireless
+ * F9:	KEY_CONFIG		Control panel / settings [TODO: Win-F21, Config]
  * F10:	KEY_SEARCH		Search
- * F11:	KEY_FN_F11		View open applications (3 boxes)
+ * F11:	KEY_FN_F11		View open applications (3 boxes) [TODO: Ctrl-Alt-Tab, F11]
  * F12:	KEY_FN_F12		Open My computer (6 boxes)
  */
 
@@ -42,6 +43,30 @@ static int tpcompactkbd_input_mapping(struct hid_device *hdev,
 		struct hid_input *hi, struct hid_field *field,
 		struct hid_usage *usage, unsigned long **bit, int *max)
 {
+	if ((usage->hid & HID_USAGE_PAGE) == HID_UP_CONSUMER) {
+		set_bit(EV_REP, hi->input->evbit);
+		switch (usage->hid & HID_USAGE) {
+		case 0x0070:	tpckbd_map_key_clear(KEY_BRIGHTNESSDOWN);	return 1;
+		case 0x006f:	tpckbd_map_key_clear(KEY_BRIGHTNESSUP);	return 1;
+		case 0x03f1:	tpckbd_map_key_clear(KEY_FN_F8);	return 1;
+		case 0x0221:	tpckbd_map_key_clear(KEY_SEARCH);	return 1;
+		case 0x03f2:	tpckbd_map_key_clear(KEY_FN_F12);	return 1;
+		}
+	}
+
+	if ((usage->hid & HID_USAGE_PAGE) == HID_UP_MSVENDOR) {
+		set_bit(EV_REP, hi->input->evbit);
+		switch (usage->hid & HID_USAGE) {
+		case 0x00f1:	tpckbd_map_key_clear(KEY_MICMUTE);	return 1;
+		case 0x00f4:	tpckbd_map_key_clear(KEY_SWITCHVIDEOMODE);	return 1; /* Down */
+		case 0x00f5:	tpckbd_map_key_clear(KEY_SWITCHVIDEOMODE);	return 1; /* Up */
+		case 0x00f6:	tpckbd_map_key_clear(KEY_CONFIG);	return 1; /* Down */
+		case 0x00f7:	tpckbd_map_key_clear(KEY_CONFIG);	return 1; /* Up */
+		case 0x00f8:	tpckbd_map_key_clear(KEY_FN_F11);	return 1; /* Down */
+		case 0x00f9:	tpckbd_map_key_clear(KEY_FN_F11);	return 1; /* Up */
+		}
+	}
+
 	return 0;
 }
 
